894fa495b78a8aab4635bda6cba3ee4528bf944e
nobusuke007 <nobusuke007@gmail.com>
2024-12-16 14:36:01 +0900

    menuの追加に伴う整理

---
+++ menu.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Helpみぃ〜！")
+st.write("幹事を任された者がいる。")
+st.write("幹事に背く参加者がいる。")
+st.write("それは飲み会幹事2000年の宿命。")
+st.write("見よ。")
+st.write("今、この永き幹事業務の歴史に終止符が打たれる……")
